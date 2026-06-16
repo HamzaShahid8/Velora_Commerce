5db7057323cedca09ad5096f091c7b1d93b78e49
Hamza Shahid <hamzishahid8@gmail.com>
2026-06-16 14:01:37 +0500

    fix vercel index files

--- backend/index.py
+++ backend/index.py
@@ -2,14 +2,12 @@ import os
 import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent
-DJANGO_PROJECT_DIR = BASE_DIR / "Velora"
+BASE_DIR = Path(__file__).resolve().parent / "Velora"
 
-sys.path.insert(0, str(DJANGO_PROJECT_DIR))
+sys.path.insert(0, str(BASE_DIR))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Velora.settings")
 
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
-app = application
