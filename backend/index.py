0e19d728d6e3be0f7225fbb2b4612825825e9ea6
Hamza Shahid <hamzishahid8@gmail.com>
2026-06-16 13:51:47 +0500

    fix vercel

--- backend/index.py
+++ backend/index.py
@@ -2,7 +2,7 @@ import os
 import sys
 from pathlib import Path
 
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent
 DJANGO_PROJECT_DIR = BASE_DIR / "Velora"
 
 sys.path.insert(0, str(DJANGO_PROJECT_DIR))
